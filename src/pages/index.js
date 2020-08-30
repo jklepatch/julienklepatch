769b880dfb6f28e8d7f1afb590f6f7b4d3940213
Julien Klepatch <julien@julienklepatch.com>
2020-08-31 03:33:55 +0800

    update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Julien Klepatch Blog"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -17,17 +17,9 @@ class IndexPage extends React.Component {
         />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
+          Blog of Julien Klepatch
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
+        <p>I am in the process of migrating my blog to a new CMS. If you want to contact me, send me an email at julien [at] julienklepatch [dot] com</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
